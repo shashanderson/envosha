daddbb82d44dfa67459c7f6b8d059da9233f175a
admin <admin@cem.com>
2017-04-10 18:25:34 +0800

    First commit

--- cem_reports/urls.py
+++ cem_reports/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.form_list, name='form_list'),
+     url(r'^$', views.form_list, name='form_list'),
+    # url(r'^$', views.blank, name='blank'),
+    url(r'^', views.form, name='form'),
+    url(r'^form_validation/', views.form_validation, name='form_validation$'),
 ]
--- cem_reports/views.py
+++ cem_reports/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 
 def form_list(request):
     return render(request, 'cem_reports/form_list.html', {})
+
+
+def form_validation(request):
+    return render(request, 'cem_reports/form_validation.html', {})
+
+
+def form(request):
+    return render(request, 'cem_reports/form.html', {})
+
+
+
