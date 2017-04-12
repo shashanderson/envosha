233c4d11b481494dbb914de29dbe3ff9c19ec58d
shashanderson <shash4rock@gmail.com>
2017-04-13 00:01:02 +0800

    My Django Girls app, first commit

--- cem_reports/urls.py
+++ cem_reports/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-     url(r'^$', views.form_list, name='form_list'),
+    url(r'^$', views.login_page, name='login_page'),
+    url(r'^$', views.form_list, name='form_list'),
     # url(r'^$', views.blank, name='blank'),
     url(r'^', views.form, name='form'),
     url(r'^form_validation/', views.form_validation, name='form_validation$'),
--- cem_reports/views.py
+++ cem_reports/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# log/views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+# this login required decorator is to not allow to any
+# view without authenticating
+@login_required(login_url="login/")
+def login(request):
+    return render(request, "form_list.html")
+
 
 def form_list(request):
     return render(request, 'cem_reports/form_list.html', {})
@@ -15,4 +27,5 @@ def form(request):
     return render(request, 'cem_reports/form.html', {})
 
 
-
+def login_page(request):
+    return render(request, 'cem_reports/login.html', {})
