b8cc0a96c7baeab06f2b36692da0e5716fba4933
shashanderson <shash4rock@gmail.com>
2017-04-13 22:31:56 +0800

    tring againt

--- cem_reports/urls.py
+++ cem_reports/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.login_page, name='login_page'),
-    url(r'^$', views.form_list, name='form_list'),
+    #url(r'^form_list/$', views.form_list, name='form_list'),
     # url(r'^$', views.blank, name='blank'),
-    url(r'^', views.form, name='form'),
+    #url(r'^', views.form, name='form'),
     url(r'^form_validation/', views.form_validation, name='form_validation$'),
 ]
--- cem_reports/views.py
+++ cem_reports/views.py
@@ -2,18 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# log/views.py
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-# Create your views here.
-# this login required decorator is to not allow to any
-# view without authenticating
-@login_required(login_url="login/")
-def login(request):
-    return render(request, "form_list.html")
-
 
 def form_list(request):
     return render(request, 'cem_reports/form_list.html', {})
@@ -26,6 +14,6 @@ def form_validation(request):
 def form(request):
     return render(request, 'cem_reports/form.html', {})
 
-
 def login_page(request):
     return render(request, 'cem_reports/login.html', {})
+
